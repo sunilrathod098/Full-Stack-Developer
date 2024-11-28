6c4dc08fa726dde4adf8763ed107f9083f945bc6
sunilrathod098 <sunilrajputhrathod@gmail.com>
2024-11-28 12:09:17 +0530

    Removed unnecessary models and updated main.jsx

--- ReactJs/ReactWithVite/src/main.jsx
+++ ReactJs/ReactWithVite/src/main.jsx
@@ -2,8 +2,13 @@
 import ReactDOM from 'react-dom'
 import App from './App.jsx'
 
+//this with React + Vite
 ReactDOM.createRoot(document.getElementById('root')).render(
-
     <App />
-
 )
+
+// // this is React
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <App />
+// )
